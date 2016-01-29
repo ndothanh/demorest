487bb260e79adc77bd7bfc9e63fc6d60713cf3c4
unknown <ndothanh@gmail.com>
2016-01-29 23:41:26 +0700

    First commit

---
+++ src/main/java/com/aphab/RestStsApplication.java
@@ -0,0 +1,12 @@
+package com.aphab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestStsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestStsApplication.class, args);
+	}
+}
