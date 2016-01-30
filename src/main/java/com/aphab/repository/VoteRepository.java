a25413f8622eed036c7bfd9c02ae12ab95bea025
ndothanh <ndothanh@gmail.com>
2016-01-30 15:44:21 +0700

    finish Rest on controller

--- src/main/java/com/aphab/repository/VoteRepository.java
+++ src/main/java/com/aphab/repository/VoteRepository.java
@@ -1,8 +1,12 @@
 package com.aphab.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import com.aphab.domain.Vote;
 
 public interface VoteRepository extends CrudRepository<Vote, Long> {
+	
+	@Query(value="select v.* from Option o, Vote v where o.POLL_ID = ?1 and	v.OPTION_ID = o.OPTION_ID", nativeQuery = true)
+			public Iterable<Vote> findByPoll(Long pollId);
 
 }
